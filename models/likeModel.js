41a680673fbeabea79f6cc4200c97eb38636be1e
Emox133 <emir.betaacc0unt@gmail.com>
2020-03-28 14:16:01 +0100

    Added unique like pre save middleware

--- models/likeModel.js
+++ models/likeModel.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+const AppError = require('./../utils/AppError')
 
 const likeSchema = new mongoose.Schema({
     owner: {
         type: mongoose.Schema.ObjectId,
         ref: 'User',
         required: [true, 'Like must belong to some user.'],
-        unique: true
+        unique: true,
+        sparse: true
     },
     belongsTo: {
         type: mongoose.Schema.ObjectId,
@@ -13,6 +15,16 @@ const likeSchema = new mongoose.Schema({
         required: [true, 'Like must belong to some post.']
     }
 });
+
+likeSchema.pre('save', function (next) {
+    Like.find({owner : this.owner, belongsTo: this.belongsTo}, function (err, docs) {
+        if (!docs.length){
+            next();
+        }else{                
+            next(new AppError('You already liked this post. 💥', 400));
+        }
+    });
+}) ;
 
 const Like = mongoose.model('Like', likeSchema);
 
